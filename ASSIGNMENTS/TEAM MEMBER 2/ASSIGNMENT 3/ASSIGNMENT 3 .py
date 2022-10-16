d069f837536d7df8c5f2433372c2394486899229
curiemanoharan <113848432+curiemanoharan@users.noreply.github.com>
2022-10-16 23:41:49 +0530

    Add files via upload

---
+++ ASSIGNMENTS/TEAM MEMBER 2/ASSIGNMENT 3/ASSIGNMENT 3 .py
@@ -0,0 +1,63 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================================================= RESTART: C:\Users\curie\AppData\Local\Programs\Python\Python310\ass 3.py =======================================================
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
+Blink!!
+Wait for Signal -  Ready to go..
+Blink!!
+Blink!!
+Stop -  Stop behind zebra cross..
+Blink!!
+Blink!!
+Move - You can go..
+Blink!!
